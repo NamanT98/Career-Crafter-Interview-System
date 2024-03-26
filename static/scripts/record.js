1b1ea0bf2f0e9de97ac62b0b47673445d25290f7
NamanT98 <namant@iitbhilai.ac.in>
2024-03-26 15:08:11 +0530

    added camera stream

--- static/scripts/record.js
+++ static/scripts/record.js
@@ -1,6 +1,11 @@
-const webCamContainer = document.getElementById("web-cam-container");
+const webcamElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("canvas");
+const webcam = new Webcam(webcamElement);
+webcam.start();
 
 let chunks = [];
+
+let recordedMedia = null;
 
 const audioMediaConstraints = {
   audio: true,
@@ -13,13 +18,16 @@ const videoMediaConstraints = {
 };
 
 function startRecording(thisButton, otherButton) {
-  // Access the camera and microphone
+  // Access the microphone
+
   navigator.mediaDevices
     .getUserMedia(audioMediaConstraints)
     .then((mediaStream) => {
+      console.log(mediaStream);
       const mediaRecorder = new MediaRecorder(mediaStream);
       window.mediaStream = mediaStream;
       window.mediaRecorder = mediaRecorder;
+
       mediaRecorder.start();
       mediaRecorder.ondataavailable = (e) => {
         chunks.push(e.data);
@@ -27,18 +35,18 @@ function startRecording(thisButton, otherButton) {
 
       mediaRecorder.onstop = () => {
         const blob = new Blob(chunks, {
-          type: "audio/mpeg",
+          type: "audio/wav",
         });
         chunks = [];
 
-        const recordedMedia = document.createElement("audio");
+        recordedMedia = document.createElement("audio");
         recordedMedia.controls = true;
 
         const recordedMediaURL = URL.createObjectURL(blob);
 
         recordedMedia.src = recordedMediaURL;
 
-        // document.getElementById(`vid-recorder`).append(recordedMedia);
+        document.getElementById(`vid-recorder`).append(recordedMedia);
       };
 
       document.getElementById(`vid-record-status`).innerText = "Recording";
